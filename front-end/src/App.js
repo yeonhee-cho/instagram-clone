b9e407af098f1686aa5bca686cff2c15cad3a26b
조연희 <yhcho.proj@gmail.com>
2025-12-04 18:20:19 +0900

    환경설정

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// ============================================
+// src/App.js
+// TODO: React Router 설정하기
+// - BrowserRouter, Routes, Route import 하기
+// - LoginPage, FeedPage, UploadPage import 하기
+// - PrivateRoute import 하기
+// - /login 경로에 LoginPage 연결
+// - /feed 경로에 FeedPage 연결 (PrivateRoute로 보호)
+// - /upload 경로에 UploadPage 연결 (PrivateRoute로 보호)
+// - 기본 경로(/)는 /login으로 리다이렉트
+// ============================================
+
+import React from 'react';
 import './App.css';
 
+// TODO: 필요한 컴포넌트들을 import 하세요
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/* TODO: Router 설정을 완성하세요 */}
+        </div>
+    );
 }
 
 export default App;
---
+++ front-end/src/provider/PrivateRoute.js
@@ -0,0 +1,21 @@
+// ============================================
+// src/provider/PrivateRoute.js
+// TODO: 로그인 확인 후 페이지 접근 제어
+// - localStorage에서 'token' 가져오기
+// - token이 없으면 /login으로 리다이렉트
+// - token이 있으면 children(자식 컴포넌트) 렌더링
+// ============================================
+
+import React from 'react';
+
+const PrivateRoute = ({ children }) => {
+    // TODO: localStorage에서 token을 가져오세요
+
+    // TODO: token이 없으면 /login으로 리다이렉트 하세요
+
+    // TODO: token이 있으면 children을 반환하세요
+
+    return null;
+};
+
+export default PrivateRoute;
